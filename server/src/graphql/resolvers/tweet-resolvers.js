5beb1b551cbc1d16f87b854b5175ff3c0cdd6d43
bkdev98 <bkdev98@gmail.com>
2017-08-02 11:00:00 +0700

    combine tweet and user

--- server/src/graphql/resolvers/tweet-resolvers.js
+++ server/src/graphql/resolvers/tweet-resolvers.js
@@ -13,7 +13,15 @@ export default {
   getTweets: async (_, args, { user }) => {
     try {
       await requireAuth(user);
-      Tweet.find({}).sort({ createdAt: -1 });
+      return Tweet.find({}).sort({ createdAt: -1 });
+    } catch (error) {
+      throw error;
+    }
+  },
+  getUserTweets: async(_, args, { user }) => {
+    try {
+      await requireAuth(user);
+      return Tweet.find({ user: user._id }).sort({ createdAt: -1 });
     } catch (error) {
       throw error;
     }
@@ -21,7 +29,7 @@ export default {
   createTweet: async (_, args, { user }) => {
     try {
       await requireAuth(user);
-      return Tweet.create(args);
+      return Tweet.create({ ...args, user: user._id });
     } catch (error) {
       throw error;
     }
@@ -29,7 +37,14 @@ export default {
   updateTweet: async (_, { _id, ...rest }, { user }) => {
     try {
       await requireAuth(user);
-      return Tweet.findByIdAndUpdate(_id, rest, { new: true });
+      const tweet = await Tweet.findOne({ _id, user: user._id });
+      if (!tweet) {
+        throw new Error('Not found!');
+      }
+      Object.entries(rest).forEach(([key, value]) => {
+        tweet[key] = value;
+      });
+      return tweet.save();
     } catch (error) {
       throw error;
     }
@@ -37,7 +52,11 @@ export default {
   deleteTweet: async (_, { _id }, { user }) => {
     try {
       await requireAuth(user);
-      await Tweet.findByIdAndRemove(_id);
+      const tweet = await Tweet.findOne({ _id, user: user._id });
+      if (!tweet) {
+        throw new Error('Not found!');
+      }
+      await tweet.remove();
       return {
         message: 'Delete Success!'
       }
--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,7 @@ import notifier from 'node-notifier';
 import './config/db';
 import constants from './config/constants';
 import middlewares from './config/middlewares';
-import mocks from './mocks';
+// import mocks from './mocks';
 
 const app = express();
 
@@ -15,7 +15,7 @@ middlewares(app);
 
 const graphQLServer = createServer(app);
 
-mocks().then(() => {
+// mocks().then(() => {
   graphQLServer.listen(constants.PORT, err => {
     if (err) {
       notifier.notify({
@@ -31,4 +31,4 @@ mocks().then(() => {
       console.log(`App listen to port: ${constants.PORT} 🐠`);
     }
   });
-});
+// });
