e7e2cca413bf5eb2e19c1777aae350a4531651b1
Quốc Khánh <bkdev98@gmail.com>
2017-08-27 11:18:12 +0700

    create tweetAdded subscription

--- server/src/config/constants.js
+++ server/src/config/constants.js
@@ -3,4 +3,5 @@ export default {
   DB_URL: 'mongodb://localhost/tweet-development',
   GRAPHQL_PATH: '/graphql',
   JWT_SECRET: '부이탄콰',
+  SUBSCRIPTIONS_PATH: '/subscriptions',
 };
--- server/src/index.js
+++ server/src/index.js
@@ -2,16 +2,40 @@
 
 import express from 'express';
 import { createServer } from 'http';
+import { graphiqlExpress, graphqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+import { SubscriptionServer } from 'subscriptions-transport-ws';
+import { execute, subscribe } from 'graphql';
 import notifier from 'node-notifier';
 
 import './config/db';
 import constants from './config/constants';
 import middlewares from './config/middlewares';
+import typeDefs from './graphql/schema';
+import resolvers from './graphql/resolvers';
 // import mocks from './mocks';
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
 
 const app = express();
 
 middlewares(app);
+
+app.use(
+  '/graphiql',
+  graphiqlExpress({
+    endpointURL: constants.GRAPHQL_PATH,
+    subscriptionsEndpoint: `ws://localhost:${constants.PORT}${constants.SUBSCRIPTIONS_PATH}`,
+}));
+app.use(constants.GRAPHQL_PATH, graphqlExpress(req => ({
+  schema,
+  context: {
+    user: req.user
+  }
+})));
 
 const graphQLServer = createServer(app);
 
@@ -24,6 +48,15 @@ const graphQLServer = createServer(app);
       });
       console.error(err);
     } else {
+      new SubscriptionServer({ // eslint-disable-line
+        schema,
+        execute,
+        subscribe
+      }, {
+        server: graphQLServer,
+        path: constants.SUBSCRIPTIONS_PATH,
+      });
+
       notifier.notify({
         'title': 'Inspery API',
         'message': '👨‍❤️‍💋‍👨 Service started!',
